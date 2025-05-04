528b9e2fc96f59ac8229840d756980acb62edcbd
VEIDD <separik3@gmail.com>
2025-05-04 16:29:05 +0300

    return

--- projects/Anime/app.js
+++ projects/Anime/app.js
@@ -3,11 +3,6 @@ let page = 1
 let url = ``
 let text_anime = document.querySelector('.title')
 
-const url2 = new URL(window.location.href);
-url2.searchParams.set('page', 1);
-history.pushState({}, '', url2);
-
-
 const hasVerticalScroll = window.innerHeight > document.body.scrollHeight;
 if(hasVerticalScroll){
 	document.querySelector('main').style = `height:${window.innerHeight}px;`
